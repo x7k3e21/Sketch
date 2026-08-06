88c89fe6bf59c138d69cfad8d27980a8b2a28e21
x7k3e21 <uly4nkin.denis@gmail.com>
2026-08-06 19:03:26 +0300

    Added basic Application class to Sketch.Core

---
+++ Sketch.Core/include/Sketch/Core/Application.hpp
@@ -0,0 +1,16 @@
+
+#ifndef SK_CORE_APPLICATION_HPP
+#define SK_CORE_APPLICATION_HPP
+
+namespace sk
+{
+    class Application
+    {
+        public:
+
+        Application();
+        virtual ~Application();
+    };
+}
+
+#endif
---
+++ Sketch.Core/source/Application.cpp
@@ -0,0 +1,15 @@
+
+#include "Sketch/Core/Application.hpp"
+
+namespace sk
+{
+    Application::Application()
+    {
+
+    }
+
+    Application::~Application()
+    {
+        
+    }
+}
